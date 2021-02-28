2835d8c5e8749cf9f98fb669a3e87779c2acd853
AadilBR <aadil.iphone@gmail.com>
2021-02-28 09:33:27 +0100

    exo-expressions-conditions

---
+++ welcome.js
@@ -0,0 +1,16 @@
+/* let FirstName1 = 'Alice';
+let genderFemale = false;
+
+console.log(`Bonjour ${FirstName1}, vous êtes entré${(genderFemale ? ("") : ("e"))} sur le chan`);
+
+
+let FirstName2 = 'Bob';
+let genderMale = true;
+
+console.log(`Bonjour ${FirstName2}, vous êtes entré${(genderMale ? ("") : ("e"))} sur le chan`)
+ */
+
+let Firstname = 'BoB'
+let Gender = 'Male'
+
+console.log(`Bonjour ${Firstname}, vous êtes entré${Gender === 'Female' ? 'e' : ''} sur le chan`)
